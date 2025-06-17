682abb3a0838c92ae6c4b1e248d998e371368bb6
cluzier <connerluzier@outlook.com>
2025-06-17 18:52:38 -0400

    feat: Add weather-based watering advice and geolocation support

---
+++ PlantTrackerApp.Server/Models/WeatherResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace PlantTrackerApp.Server.Models
+{
+    public class WeatherResponse
+    {
+        public required Daily daily { get; set; }
+
+        public class Daily
+        {
+            public required List<string> time { get; set; }
+            public required List<double> precipitation_sum { get; set; }
+        }
+    }
+}
