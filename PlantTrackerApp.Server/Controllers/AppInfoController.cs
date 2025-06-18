3be3ce44cecfb291e34ce04251f78e6002c762b3
cluzier <connerluzier@outlook.com>
2025-06-18 11:26:25 -0400

    feat: Implement versioning feature with AppInfoService and AppInfoController - cleaned up some code - validation for name and species fields when creating a new plant; restricting numbers in text boxes

---
+++ PlantTrackerApp.Server/Controllers/AppInfoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using PlantTrackerApp.Server.Models;
+
+namespace PlantTrackerApp.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AppInfoController : ControllerBase
+    {
+        private readonly IOptions<AppConfig> _config;
+
+        public AppInfoController(IOptions<AppConfig> config)
+        {
+            _config = config;
+        }
+
+        [HttpGet("version")]
+        public IActionResult GetVersion()
+        {
+            return Ok(new { version = _config.Value.AppVersion });
+        }
+    }
+}
